e1b486633131b7d4ce98bae30aaadabc7ab56931
Lilian Yared Sanchez Hernandez <168390254+lilisan08@users.noreply.github.com>
2024-06-06 12:31:28 -0600

    Add files via upload

--- programa12.cpp
+++ programa12.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    int numero = 1;
 
-    while(numero <= 100) {
-        std::cout << numero;
-        numero += 2;
-    }
+int num1,num2,total;
 
-    return 0;
+cout<<"dame el primer numero:";
+cin>>num1;
+cout<<"dame el segundo numero:";
+cin>>num2;
+total=num1%num2;
+cout<<"el total de la division es "<<total;
+if(total-0){;
+cout<<" el numero no es divisible";
+}
+else{
+cout<<"el numero es divisible";
+
+
+}
+return 0;
 }
--- programa13.cpp
+++ programa13.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    int numero = 1;
+    int x;
 
-    while(numero <= 100) {
-        std::cout << numero;
-        numero += 2;
+    cout << "Ingresa un número: ";
+    cin >> x;
+
+    if (x % 2 == 0) {
+        cout << "El número " << x << " es par";
+    } else {
+        cout << "El número " << x << " no es par";
+    }
+
+    if (x % 3 == 0) {
+        cout << "El número " << x << " es divisible de 3";
+    } else {
+        cout << "El número " << x << " no es divisible de 3";
     }
 
     return 0;
